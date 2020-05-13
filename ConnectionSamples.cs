b594fac71b1c8c02c0255e2cce4aa0ad499efe29
lmiller58 <41927962+lmiller58@users.noreply.github.com>
2020-05-13 13:10:21 -0500

    added proxy creation sample

---
+++ ConnectionSamples.cs
@@ -0,0 +1,34 @@
+﻿using kCura.Relativity.Client;
+using System;
+using Relativity.Services.ServiceProxy;
+
+namespace Refactoring_Samples
+{
+    class ConnectionSamples
+    {
+        public IRSAPIClient getIRSAPIClient()
+        {
+            Uri ROneEndpoint = new Uri("{my_company_name}/Relativity.services");
+            IRSAPIClient rsapiClient = new RSAPIClient(ROneEndpoint, new kCura.Relativity.Client.UsernamePasswordCredentials("username", "password));
+            return rsapiClient;
+        }
+
+        public ServiceFactory getNewServiceFactory()
+        {
+
+            // DevVm Address
+            String restServerAddress = "{my_company_name}/Relativity.Rest/Api";
+            string rsapiAddress = "{my_company_name}/Relativity.Services";
+
+            Uri keplerUri = new Uri(restServerAddress);
+            Uri servicesUri = new Uri(rsapiAddress);
+
+            ServiceFactorySettings settings = new ServiceFactorySettings(
+                servicesUri, keplerUri, new Relativity.Services.ServiceProxy.UsernamePasswordCredentials("username", "password"));
+
+            Relativity.Services.ServiceProxy.ServiceFactory _serviceFactory = new Relativity.Services.ServiceProxy.ServiceFactory(settings);
+
+            return _serviceFactory;
+        }
+    }
+}
